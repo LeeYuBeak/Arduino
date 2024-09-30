40a55cd2de22428cc6bbc7ed660e42a4858aeff6
LeeYuBeak <105337778+LeeYuBeak@users.noreply.github.com>
2024-10-01 01:39:43 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,26 @@
+#define PIN_LED 7
+
+void setup() {
+    pinMode(PIN_LED, OUTPUT);
+    digitalWrite(PIN_LED, 0); 
+}
+
+void loop() {
+    
+    digitalWrite(PIN_LED, 1);
+    delay(1000); // 1초 대기
+
+    
+    for (int i = 0; i < 5; i++) {
+        digitalWrite(PIN_LED, 0); 
+        delay(100);
+        digitalWrite(PIN_LED, 1); 
+        delay(100); 
+    }
+
+    
+    digitalWrite(PIN_LED, 1);
+
+  
+    while(1) {}
+}
